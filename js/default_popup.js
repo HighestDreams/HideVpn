2e101a301517b6899017e15300a7e576a20737c2
HD <79803040+HighestDreams@users.noreply.github.com>
2022-07-09 18:20:25 +0430

    Add files via upload

---
+++ js/default_popup.js
@@ -0,0 +1,32 @@
+var power_btn = document.getElementById('power');
+var status = document.getElementById('status');
+
+power_btn.addEventListener('click', onClickPower);
+
+function onClickPower() {
+    // Check status
+
+    // if its connected, Then disconnect & change status
+    // if its disconnected, Then connect & change status
+}
+
+const select = document.querySelectorAll('.selectBtn');
+const option = document.querySelectorAll('.option');
+let index = 1;
+
+select.forEach(a => {
+	a.addEventListener('click', b => {
+		const next = b.target.nextElementSibling;
+		next.classList.toggle('toggle');
+		next.style.zIndex = index++;
+	})
+})
+option.forEach(a => {
+	a.addEventListener('click', b => {
+		b.target.parentElement.classList.remove('toggle');
+		
+		const parent = b.target.closest('.select').children[0];
+		parent.setAttribute('data-type', b.target.getAttribute('data-type'));
+		parent.innerText = b.target.innerText;
+	})
+})
